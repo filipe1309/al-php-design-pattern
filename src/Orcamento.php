b03f2a5b8796376c01e9c36bcdcfa33a43bd1ac6
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-02 16:07:42 -0300

    Add desconto extra com estados

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -6,4 +6,23 @@ class Orcamento
 {
     public int $quantidadeDeItens;
     public float $valor;
+    public string $estadoAtual;
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->calculaDescontoExtra();
+    }
+
+    public function calculaDescontoExtra(): float
+    {
+        if ($this->estadoAtual == 'EM_APROVACAO') {
+            return $this->valor * 0.05;
+        }
+
+        if ($this->estadoAtual == 'APROVADO') {
+            return $this->valor * 0.02;
+        }
+
+        throw new \DomainException('Orcamentos reprovados ou finalizados nao podem receber descontos');
+    }
 }
