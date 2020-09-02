d615d9fa676dab22b855a77103a90b7cb4a78fea
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-02 15:58:36 -0300

    Refatorando com DP Template Method

---
+++ src/Impostos/ImpostoCom2Aliquotas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\DesignPattern\Impostos;
+
+use Alura\DesignPattern\Orcamento;
+
+abstract class ImpostoCom2Aliquotas implements Imposto
+{
+    public function calculaImposto(Orcamento $orcamento): float
+    {
+        if ($this->deveAplicarATaxaMaxima($orcamento)) {
+            return $this->calculaTaxaMaxima($orcamento);
+        }
+
+        return $this->calculaTaxaMinima($orcamento);
+    }
+
+    abstract protected function deveAplicarATaxaMaxima(Orcamento $orcamento): bool;
+
+    abstract protected function calculaTaxaMaxima(Orcamento $orcamento): float;
+    
+    abstract protected function calculaTaxaMinima(Orcamento $orcamento): float;
+}
