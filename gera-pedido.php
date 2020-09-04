aba7a8a0feadbd2b664eb418269080fe4ef22d38
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-03 22:07:42 -0300

    Refatorar com DP Observer

--- gera-pedido.php
+++ gera-pedido.php
@@ -1,8 +1,10 @@
 <?php
+//$ php gera-pedido.php 1200.50 7 Filipe
 
 require_once 'vendor/autoload.php';
 
 use Alura\DesignPattern\{GerarPedido, GerarPedidoHandler};
+use Alura\DesignPattern\AcoesAoGerarPedido\{ CriarPedidoNoBanco, LogGerarPedido, EnviarPedidoPorEmail };
 
 $valorOrcamento = $argv[1];
 $numeroDeItens = $argv[2];
@@ -10,4 +12,7 @@ $nomeCliente = $argv[3];
 
 $gerarPedido = new GerarPedido($valorOrcamento, $numeroDeItens, $nomeCliente);
 $gerarPedidoHandler = new GerarPedidoHandler();
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new CriarPedidoNoBanco());
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new EnviarPedidoPorEmail());
+$gerarPedidoHandler->adicionarAcaoAoGerarPedido(new LogGerarPedido());
 $gerarPedidoHandler->execute($gerarPedido);
--- src/GerarPedidoHandler.php
+++ src/GerarPedidoHandler.php
@@ -2,16 +2,20 @@
 
 namespace Alura\DesignPattern;
 
-use Alura\DesignPattern\AcoesAoGerarPedido\{ CriarPedidoNoBanco, LogGerarPedido, EnviarPedidoPorEmail };
+use Alura\DesignPattern\AcoesAoGerarPedido\{ CriarPedidoNoBanco, LogGerarPedido, EnviarPedidoPorEmail, AcaoAposGerarPedido };
 
 class GerarPedidoHandler
 {
-    private float $valorOrcamento;
-    private int $numeroItens;
-    private string $nomeCliente;
+    /** @var AcaoAposGerarPedido[] */
+    private array $acoesAposGerarPedido = [];
 
     public function __construct(/* PedidoRepository, MailService */)
     {
+    }
+
+    public function adicionarAcaoAoGerarPedido(AcaoAposGerarPedido $acao)
+    {
+        $this->acoesAposGerarPedido[] = $acao;
     }
 
     //$ php gera-pedido.php 1200.50 7 Filipe
@@ -26,12 +30,8 @@ class GerarPedidoHandler
         $pedido->nomeCliente = $gerarPedido->getNomeCliente();
         $pedido->orcamento = $orcamento;
         
-        $pedidosRepository = new CriarPedidoNoBanco();
-        $logGerarPedido = new LogGerarPedido();
-        $enviarPedidoPorEmail = new EnviarPedidoPorEmail();
-
-        $pedidosRepository->executaAcao($pedido);
-        $logGerarPedido->executaAcao($pedido);
-        $enviarPedidoPorEmail->executaAcao($pedido);
+        foreach($this->acoesAposGerarPedido as $acao) {
+            $acao->executaAcao($pedido);
+        }
     }
 }
