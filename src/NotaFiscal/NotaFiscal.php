8c77428f8eb392ff02fafde5cb2d220199749d98
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-09 10:12:16 -0300

    Add Nota Fiscal

---
+++ src/NotaFiscal/NotaFiscal.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alura\DesignPattern\NotaFiscal;
+
+class NotaFiscal
+{
+    public string $cnpjEmpresa;
+    public string $razaoSocialEmpresa;
+    public array $itens;
+    public string $observacoes;
+    public \DateTimeInterface $dataEmissao;
+    public float $valorImpostos;
+
+
+    public function __construct(
+        string $cnpjEmpresa,
+        string $razaoSocialEmpresa,
+        array $itens,
+        string $observacoes,
+        \DateTimeInterface $dataEmissao,
+        float $valorImpostos
+    ) {
+        $this->cnpjEmpresa = $cnpjEmpresa;
+        $this->razaoSocialEmpresa = $razaoSocialEmpresa;
+        $this->itens = $itens;
+        $this->observacoes = $observacoes;
+        $this->dataEmissao = $dataEmissao;
+        $this->valorImpostos = $valorImpostos;
+    }
+
+    
+    public function valor(): float
+    {
+        return 0;
+    }
+}
