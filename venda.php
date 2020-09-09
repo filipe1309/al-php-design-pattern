b428ebc1512c1a81b4a01ca4507693fea562561f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-09 10:01:30 -0300

    Refatorar com DP Abstract Factory

---
+++ venda.php
@@ -0,0 +1,12 @@
+<?php
+
+use Alura\DesignPattern\Venda\{ VendaServicoFactory, VendaProdutoFactory };
+
+require 'vendor/autoload.php';
+
+// $fabricaVenda = new VendaServicoFactory('Bob Dylan');
+$fabricaVenda = new VendaProdutoFactory(1000);
+$venda = $fabricaVenda->criarVenda();
+$imposto = $fabricaVenda->imposto();
+
+var_dump($venda, $imposto);
